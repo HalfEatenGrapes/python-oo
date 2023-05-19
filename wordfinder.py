9d033b3525bdf0c55bb191d6eb782edf7bb62d3a
Elizabeth Mace <northwood1227@gmail.com>
2023-05-19 11:51:38 -0400

    first add

---
+++ wordfinder.py
@@ -0,0 +1,37 @@
+"""Word Finder: finds random words from a dictionary."""
+
+
+class WordFinder:
+    """returns a random word from that list of words
+    >>> wf = WordFinder("words.txt")
+    235886 words read
+
+    >>> wf.random()
+    'cat'
+
+    >>> wf.random()
+    'cat'
+
+    >>> wf.random()
+    'porcupine'
+
+    >>> wf.random()
+    'dog'
+    """
+
+    def __init__(self, path):
+        """Read dictionary and reports # items read."""
+
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words."""
+
+        return [w.strip() for w in dict_file]
+
+    def random(self):
+        """Return random word."""
+
+        return random.choice(self.words)
